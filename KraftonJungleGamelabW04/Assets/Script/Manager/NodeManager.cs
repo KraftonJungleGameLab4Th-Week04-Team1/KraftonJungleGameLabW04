2ef1bda49e7ee4ddd150c27fe2e40461b8c6ac96
sayishuu <smile52673@gmail.com>
2025-03-25 21:14:08 +0900

    [FEAT] NodeManager 사용법 명시

--- KraftonJungleGamelabW04/Assets/Script/Manager/NodeManager.cs
+++ KraftonJungleGamelabW04/Assets/Script/Manager/NodeManager.cs
@@ -1,9 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeManager : MonoBehaviour
 {
+    // 노드들을 관리하는 딕셔너리
+    public Dictionary<int, Node> NodeDic = new Dictionary<int, Node>();
+
     public void Init()
     {
-
+        GameManager.Instance.OnConfirmNodeAction += GainResourece;
     }
+
+    // Node 클래스는 구조체처럼 변수 저장용으로 사용합니다.
+    public void GainResourece(Node report)
+    {
+        int idx = GameManager.Instance.CurrentNodeIndex;
+        Node ToAircraft = new Node();
+
+        ToAircraft.Food = report.Food;
+        ToAircraft.Bolt = report.Bolt;
+        ToAircraft.Fuel = report.Fuel;
+        ToAircraft.Not = report.Not;
+        //GameManager.Aircraft.AddResources(ToAircraft);
+
+        NodeDic[idx].Food -= report.Food;
+        NodeDic[idx].Bolt -= report.Bolt;
+        NodeDic[idx].Fuel -= report.Fuel;
+        NodeDic[idx].Not -= report.Not;
+    }
+
 }
--- KraftonJungleGamelabW04/Assets/Script/Node/Node.cs
+++ KraftonJungleGamelabW04/Assets/Script/Node/Node.cs
@@ -4,20 +4,20 @@ public class Node : MonoBehaviour
 {
     [Header("노드 명")]
     [SerializeField] private string _NodeName;
-
-    private int _nodeIdx;
+    [SerializeField] private int _nodeIdx;
 
     [Header("노드 자원")]
     [SerializeField] private int _food;
     [SerializeField] private int _bolt;
-    [SerializeField] private int _not;
+    [SerializeField] private int _nut;
     [SerializeField] private int _fuel;
 
     [Header("특수 노드 여부")]
     public NodeType _nodeType;
 
-    public int Food => _food;
-    public int Bolt => _bolt;
-    public int Not => _not;
-    public int Fuel => _fuel;
+    public int NodeIdx => _nodeIdx;
+    public int Food { get => _food; set => _food = value; }
+    public int Bolt { get => _bolt; set => _bolt = value; }
+    public int Not { get => _nut; set => _nut = value; }
+    public int Fuel { get => _fuel; set => _fuel = value; }
 }
