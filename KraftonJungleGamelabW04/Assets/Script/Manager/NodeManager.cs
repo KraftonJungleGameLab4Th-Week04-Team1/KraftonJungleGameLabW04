79528a7e3b8fdc1a2c3152ee0fe301ee0a27b9dc
sayishuu <smile52673@gmail.com>
2025-03-25 21:44:15 +0900

    [FEAT] 기체손상, 우주정거장빌드업, 노드위험도재설정 메서드 구현 및 구독
    Node 클래스 오타 수정, Node 클래스에 risk 변수 추가

--- KraftonJungleGamelabW04/Assets/Script/Manager/NodeManager.cs
+++ KraftonJungleGamelabW04/Assets/Script/Manager/NodeManager.cs
@@ -8,25 +8,60 @@ public class NodeManager : MonoBehaviour
 
     public void Init()
     {
-        GameManager.Instance.OnConfirmNodeAction += GainResourece;
+        GameManager.Instance.OnConfirmNodeAction += GainResource;
+        GameManager.Instance.OnConfirmNodeAction += BuildUpSpaceStation;
+        GameManager.Instance.OnMoveNodeAction += GetDamageOnAircraft;
+        GameManager.Instance.OnMoveNodeAction += SetNodeRisk;
     }
 
     // Node 클래스는 구조체처럼 변수 저장용으로 사용합니다.
-    public void GainResourece(Node report)
+    public void GainResource(Node report)
     {
-        int idx = GameManager.Instance.CurrentNodeIndex;
+        int currentIdx = GameManager.Instance.CurrentNodeIndex;
         Node ToAircraft = new Node();
 
         ToAircraft.Food = report.Food;
         ToAircraft.Bolt = report.Bolt;
         ToAircraft.Fuel = report.Fuel;
-        ToAircraft.Not = report.Not;
+        ToAircraft.Nut = report.Nut;
         //GameManager.Aircraft.AddResources(ToAircraft);
 
-        NodeDic[idx].Food -= report.Food;
-        NodeDic[idx].Bolt -= report.Bolt;
-        NodeDic[idx].Fuel -= report.Fuel;
-        NodeDic[idx].Not -= report.Not;
+        NodeDic[currentIdx].Food -= report.Food;
+        NodeDic[currentIdx].Bolt -= report.Bolt;
+        NodeDic[currentIdx].Fuel -= report.Fuel;
+        NodeDic[currentIdx].Nut -= report.Nut;
     }
 
+    public void GetDamageOnAircraft(int nextNodeIdx)
+    {
+        // 에어 크래프트 변수 혹은 함수 나오면 추가
+        // Aircraft.Hp -= NodeDic[nextNodeIdx].Risk;
+    }
+
+    public void BuildUpSpaceStation(Node report)
+    {
+        // 원래는 UI단에서 해야하는 예외처리를 일단 여기서 처리합니다.
+        int currentIdx = GameManager.Instance.CurrentNodeIndex;
+        if (NodeDic[currentIdx].NodeType == NodeType.SpaceNode)
+        {
+            return;
+        }
+
+        //Aircraft의 자원소모에 대한 기능 구독은 AircraftManager 혹은 InputManager에서 계산하여 추가 구독합니다.
+        //이 함수에서 한번에 처리한다면 아래와 같이 구현합니다.
+        //Node ToAircraft = new Node();
+        //ToAircraft.Bolt = -report.Bolt;
+        //ToAircraft.Nut = -report.Nut;
+        
+        NodeDic[GameManager.Instance.CurrentNodeIndex].SpaceStationLevel++;
+    }
+
+    // 이동 확정 시 노드들의 기본 리스크(이벤트 적용 전)를 재설정합니다.    
+    public void SetNodeRisk(int nextNodeIdx = 0)
+    {
+        foreach (var node in NodeDic)
+        {
+            node.Value.Risk = Random.Range(0, 5);
+        }
+    }
 }
--- KraftonJungleGamelabW04/Assets/Script/Node/Node.cs
+++ KraftonJungleGamelabW04/Assets/Script/Node/Node.cs
@@ -12,12 +12,20 @@ public class Node : MonoBehaviour
     [SerializeField] private int _nut;
     [SerializeField] private int _fuel;
 
+    [Header("가변 변수")]
+    [SerializeField] private int _risk;
+
     [Header("특수 노드 여부")]
-    public NodeType _nodeType;
+    private NodeType _nodeType;
+    private int _spaceStationLevel;
 
     public int NodeIdx => _nodeIdx;
+    public string NodeName => _NodeName;
+    public NodeType NodeType => _nodeType;
     public int Food { get => _food; set => _food = value; }
     public int Bolt { get => _bolt; set => _bolt = value; }
-    public int Not { get => _nut; set => _nut = value; }
+    public int Nut { get => _nut; set => _nut = value; }
     public int Fuel { get => _fuel; set => _fuel = value; }
+    public int Risk { get => _risk; set => _risk = value; }
+    public int SpaceStationLevel { get => _spaceStationLevel; set => _spaceStationLevel = value; }
 }
